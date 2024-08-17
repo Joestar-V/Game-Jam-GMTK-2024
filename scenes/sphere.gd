e0130f462a538456411f07a87967e28fa8e22e77
Joestar-V <stellarferro@gmail.com>
2024-08-17 12:28:16 -0700

    wasted lots of time by adding a 3d rotating godot sphere into a 2d frickin game
    what is wrong with me

---
+++ scenes/sphere.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	$MeshInstance3D.rotation.y += .01
