5f63552fe9e2587ef62e82835563a75d7b832eec
Joestar-V <stellarferro@gmail.com>
2024-08-17 18:14:49 -0700

    fixed stretching cooldown

--- code/CharacterBody2D.gd
+++ code/CharacterBody2D.gd
@@ -6,24 +6,34 @@ const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var stretched
-
+var stretched = false
+var stretched_y = false
+var stretched_x = false
+var cooldown = false
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if Input.is_action_pressed("horizontal") :
+	if Input.is_action_pressed("horizontal") && Input.is_action_pressed("shift") && stretched_y == false && cooldown == false:
 		stretched = true
-		scale.x = 1
+		stretched_x = true
+		scale.x = 2
 		return
-	elif Input.is_action_pressed("vertical") :
+	elif Input.is_action_pressed("vertical") && Input.is_action_pressed("shift") && stretched_x == false && cooldown == false:
 		stretched = true
-		scale.y = 1
+		stretched_y = true
+		scale.y = 2
 		return
 	else :
+		if(stretched == true):
+			cooldown = true
+			 
+			$Timer.start()
 		stretched = false
-		scale.x = .5
-		scale.y = .5
+		stretched_y = false
+		stretched_x = false
+		scale.x = .3
+		scale.y = .3
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -41,3 +51,7 @@ func _physics_process(delta):
 #func _input(event : InputEvent): 
 	#if(event.is_action_pressed("ui_down") && is_on_floor):
 	#	position.y += 1 
+
+
+func _on_timer_timeout():
+	cooldown = false
--- scenes/heart.gd
+++ scenes/heart.gd
@@ -9,4 +9,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(health <= 0):
-		print("gameover")
+		pass
