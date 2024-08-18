1395c12608fd9fd897af89fa92e7e670f715ef76
Joestar-V <stellarferro@gmail.com>
2024-08-17 18:59:42 -0700

    better controls

--- code/CharacterBody2D.gd
+++ code/CharacterBody2D.gd
@@ -10,16 +10,14 @@ var stretched = false
 var stretched_y = false
 var stretched_x = false
 var cooldown = false
+var spin = false
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	if Input.is_action_pressed("horizontal") && Input.is_action_pressed("shift") && stretched_y == false && cooldown == false:
+	if Input.is_action_pressed("horizontal")  && stretched_y == false && cooldown == false:
 		stretched = true
 		stretched_x = true
 		scale.x = 2
 		return
-	elif Input.is_action_pressed("vertical") && Input.is_action_pressed("shift") && stretched_x == false && cooldown == false:
+	elif Input.is_action_pressed("vertical")  && stretched_x == false && cooldown == false:
 		stretched = true
 		stretched_y = true
 		scale.y = 2
@@ -34,18 +32,38 @@ func _physics_process(delta):
 		stretched_x = false
 		scale.x = .3
 		scale.y = .3
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if(Input.is_action_just_pressed("ui_accept") and is_on_floor() and velocity.x > 0.0 and Input.is_action_pressed("ui_left")):
+		velocity.x = -velocity.x
 		velocity.y = JUMP_VELOCITY
-
+		spin = true
+	elif(Input.is_action_just_pressed("ui_accept") and is_on_floor() and velocity.x < 0.0 and Input.is_action_pressed("ui_right")):
+		velocity.x = - velocity.x
+		velocity.y = JUMP_VELOCITY
+		spin = true
+	elif Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	elif is_on_floor():
+		spin = false
+		rotation = 0
+	
+	if spin :
+		rotation += .5
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
+	if(velocity.x > 0.0 and Input.is_action_pressed("ui_left")):
+		$Timer2.start
+		
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 
 #func _input(event : InputEvent): 
